45f3653f2994128a3d15d15a5d7ac0300a350034
abantaki <abantaki123@gmail.com>
2025-06-01 18:34:24 +0200

    updated url for events

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 export async function getEvents() {
-  const url = import.meta.env.VITE_BOOKINGS_URL ?? 'ABAN';
+  const url = import.meta.env.VITE_EVENTS_URL ?? "ABAN-EVENTS-URL";
   const response = await fetch(url);
   const data = await response.json();
   console.log(data);
